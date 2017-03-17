85d8b76ba9e3d38eca46abb5e56ce883e107d84b
njfloeder <njfloeder@viewlincsci10.ad.csbsju.edu>
2017-03-17 16:21:02 -0500

    new changes

--- LANN-Party_Project/src/Project/Admin_Controler.java
+++ LANN-Party_Project/src/Project/Admin_Controler.java
@@ -9,7 +9,15 @@ package Project;
  */
 public class Admin_Controler {
 
+	private DatabaseController dc;
 	public Object[] schools;
+	/**
+	 * constructor
+	 */
+	public Admin_Controler(){
+		dc = new DatabaseController();
+	}
+	
 	/**
 	 * displays the users profile
 	 * 
@@ -27,7 +35,7 @@ public class Admin_Controler {
 	 * @param lName last name of user
 	 * @param pWord password for user to log in 
 	 * @param type either a user is admin or student
-	 * @param status if they are signed in or not
+	 * @param status if they aStringre signed in or not
 	 */
 	public void displayInfo(String uName, String fName, String lName, int pWord, char type, char status){
 		
@@ -45,15 +53,13 @@ public class Admin_Controler {
 	 */
 	public void saveChanges(String uName, String fName, String lName, int pWord, char type, char status){
 
-		Student temp = new Student(uName, fName, lName, pWord, type, status);
-		 = temp;
+		dc.saveChangesToUser(wsrgh);
 	}
 	/**
 	 * save a school object
 	 */
 	public void saveSchool(University university){
-		
-		= university;
+		dc.saveSchool(university);
 	}
 	/**
 	 * add a user into database
@@ -65,10 +71,9 @@ public class Admin_Controler {
 	 * @param type either a user is admin or student
 	 * @param status if they are signed in or not
 	 */
-	public void addUser(String uName, String fName, String lName, int pWord, char type, char status){
+	public void addUser(String uName, String fName, String lName, String pWord, char type, char status){
 		
-		Student newStu = new Student(uName,fName,lName,pWord, type,status);
-		.add(newStu);
+		dc.addUser(uName, fName, lName, pWord, type, status);
 	}
 	/**
 	 * checks the users username
@@ -77,13 +82,9 @@ public class Admin_Controler {
 	 * @return boolean true or false 
 	 */
 	public boolean checkUserName(String uName){
-		
-		for(Student s: ){
-			if(s.getUserName().equals(uName))
-				return true;
-		}
-		
+	if(dc.getUser(uName)==null)
 		return false;
+	return true;
 	}
 	/**
 	 * delete a user
@@ -91,8 +92,7 @@ public class Admin_Controler {
 	 * @param uName
 	 */
 	public void deleteUser(String uName){
-		
-		.remove();
+		dc.getUser(uName).setStatus('n');
 	}
 	/**
 	 * displays a school
@@ -104,7 +104,7 @@ public class Admin_Controler {
 	 * displays the schools
 	 */
 	public void displaySchools(){
-		for(University x: ){
+		for(University x: dc.getSchools()){
 		System.out.println(x);
 		}
 	}
@@ -114,9 +114,9 @@ public class Admin_Controler {
 	 * 
 	 * @return University university
 	 */
-	public University getSchool(){
+	public University getSchool(String uName){
 		
-		return null;
+		return dc.getSchool(uName);
 	}
 	/**
 	 * allows admin to edit a university
@@ -135,12 +135,9 @@ public class Admin_Controler {
 	 * @param qualityOfLiffe, csale from 1-10 of how a user would rate the quality of life
 	 * @param emphases, emphases at the school
 	 */
-	public void editSchool(String state, String name, String ocation, String control, int percentF, int SATVerbal, int SATMath, int expenses, int numberOfApp,
+	public void editSchool(String state, String name, String location, String control, int percentF, int SATVerbal, int SATMath, int expenses, int numberOfApp,
 			int percentAdmitted, int academicScale, int qualityOfLiffe, String[] emphases){
 		
-		University newU = new University( state,  name,  ocation,  control,  percentF,  SATVerbal,  SATMath,  expenses,  numberOfApp,
-				 percentAdmitted,  academicScale,  qualityOfLiffe,  emphases);
-		
-		saveSchool(newU);
+		dc.editSchool(state, name, location, control, numberOfStudentsLow, numberOfStudentsHigh, percentFemalesLow, percentFemalesHigh, satVerbalLow, satVerbalHigh, satMathLow, satMathHigh, expensesLow, expensesHigh, percentFinAidLow, percentFinAidHigh, applicantsLow, applicantsHigh, percentAdmittedLow, percentAdmittedHigh, percentEnrolledLow, percentEnrolledHigh, academScaleLow, academScaleHigh, socialScaleLow, socialScaleHigh, qualOfLifeLow, qualOfLifeHigh, emphasis1, emphasis2, emphasis3, emphasis4, emphasis5);
 	}
 }
